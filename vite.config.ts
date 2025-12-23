35306f30d1d23e3107075ddb528ae27aeace660a
cuongdmfci <cuongdm.fci@gmail.com>
2025-12-23 23:40:02 +0700

    Modify Vite config for project base path
    Updated Vite configuration to set base path for project pages.

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,31 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, '.', '');
+//     return {
+//       server: {
+//         port: 3000,
+//         host: '0.0.0.0',
+//       },
+//       plugins: [react()],
+//       define: {
+//         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+//         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+//       },
+//       resolve: {
+//         alias: {
+//           '@': path.resolve(__dirname, '.'),
+//         }
+//       }
+//     };
+// });
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/tax-2026/', // set this for project pages (https://cuongdmfci.github.io/tax-2026/)
+  plugins: [react()],
+  // keep other existing config entries if any
+})
